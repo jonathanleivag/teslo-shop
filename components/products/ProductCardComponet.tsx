4f7fe8cd488864a9c542e030e2c9180e94fd8dfa
jonathanleivag <email@jonathanleivag.cl>
2022-03-19 03:17:39 -0300

    Cambiar de imagen cuando se hace hover y efecto de carga de imagen

--- components/products/ProductCardComponet.tsx
+++ components/products/ProductCardComponet.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Image from 'next/image'
 import { IProduct } from '../../interfaces'
+import { useBlurDataURL } from '../../hooks'
 
 export interface ProductCardComponentProps {
   product: IProduct
@@ -9,14 +10,25 @@ export interface ProductCardComponentProps {
 export const ProductCardComponet: FC<ProductCardComponentProps> = ({
   product
 }) => {
+  const [onHovered, setOnHovered] = useState<boolean>(false)
+  const blurDataUrl = useBlurDataURL(100, 400 * 0.9)
+
   return (
-    <div className='w-full h-[400px] 2xl:h-[440px] flex flex-col'>
+    <div
+      className='w-full h-[400px] 2xl:h-[440px] flex flex-col'
+      onMouseEnter={() => setOnHovered(true)}
+      onMouseLeave={() => setOnHovered(false)}
+    >
       <div className='w-full h-[90%] relative'>
         <Image
           layout='fill'
-          src={`/products/${product.images[0]}`}
+          src={`/products/${product.images[onHovered ? 1 : 0]}`}
           alt={product.description}
           objectFit='contain'
+          className='transform duration-300 ease-in-out'
+          loading='lazy'
+          placeholder='blur'
+          blurDataURL={blurDataUrl}
         />
       </div>
       <div className='w-full h-[10%]'>
