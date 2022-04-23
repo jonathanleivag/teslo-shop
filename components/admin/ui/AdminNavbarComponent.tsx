8f311ffab8a6329d044b4443fdad9cbb25c1be6f
jonathanleivag <email@jonathanleivag.cl>
2022-04-23 13:09:47 -0400

    Cambio de título de nav de adminitrador

--- components/admin/ui/AdminNavbarComponent.tsx
+++ components/admin/ui/AdminNavbarComponent.tsx
@@ -15,7 +15,13 @@ export const AdminNavbarComponent: FC = () => {
       }  top-0 bg-white w-full py-3 flex flex-row justify-center items-center`}
     >
       <div className='w-[96%] flex flex-row justify-between'>
-        <div className='w-1/2 lg:w-[10%]'>
+        <div className='w-1/2 lg:w-[90%] flex flex-row gap-1'>
+          <Link href='/admin' replace passHref shallow={false}>
+            <a>
+              <span className='prose prose-base font-bold'>Dashboard </span>
+              <span className='prose prose-sm font-sans'> / </span>
+            </a>
+          </Link>
           <Link href='/' replace passHref shallow={false}>
             <a>
               <span className='prose prose-base font-bold'>Teslo |</span>
