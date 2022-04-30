ed486a281ce8efd797e0b412cda0e77892e87e24
jonathanleivag <email@jonathanleivag.cl>
2022-04-30 13:19:42 -0400

    Middleware para admin

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -4,15 +4,47 @@ import { ILogin } from '../../interfaces/loginInterface'
 
 export async function middleware (req: NextRequest, ev: NextFetchEvent) {
   const { origin } = req.nextUrl
+  const { name, params } = req.page
   const session = await getToken({
     req: req as any,
     secret: process.env.NEXTAUTH_SECRET
   })
 
   if (!session) {
-    const requestPage = req.page.params?.id
-      ? `/admin/orders/${req.page.params!.id}`
-      : '/admin/orders'
+    let requestPage = ''
+
+    if (name === '/admin') {
+      requestPage = '/admin'
+    }
+
+    if (name === '/admin/orders') {
+      requestPage = '/admin/orders'
+    }
+
+    if (name === '/admin/orders/[id] ') {
+      requestPage = '/admin/orders/' + params!.id
+    }
+
+    if (name === '/admin/orders/status/[status]') {
+      requestPage = '/admin/orders/status/' + params!.status
+    }
+
+    if (name === '/admin/users') {
+      requestPage = '/admin/users'
+    }
+
+    if (name === '/admin/products') {
+      requestPage = '/admin/products'
+    }
+
+    if (name === '/admin/products/[slug]') {
+      requestPage = '/admin/products/' + params!.slug
+    }
+
+    if (name === '/admin/products/status/[status]') {
+      requestPage = '/admin/products/status/' + params!.status
+    }
+
     return NextResponse.redirect(`${origin}/auth/login?redirect=${requestPage}`)
   }
 
