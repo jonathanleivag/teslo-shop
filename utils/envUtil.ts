ea8c1f6d6f3fbcea046eac4b5723a99567d02bd9
jonathanleivag <email@jonathanleivag.cl>
2022-05-01 13:29:01 -0400

    cambio de hora de actualizar pagina

--- utils/envUtil.ts
+++ utils/envUtil.ts
@@ -10,6 +10,6 @@ export const URL_API_GRAPHQL: string = process.env.URL_API
 export const NEXT_PUBLIC_PAYPAL_CLIENT_ID: string =
   process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || ''
 
-export const REVALIDATE: number = +process.env.REVALIDATE! || 86400000
+export const REVALIDATE: number = +process.env.REVALIDATE! || 86400
 export const NEXT_PUBLIC_REVALIDATE_DASHBOARD: number =
   +process.env.NEXT_PUBLIC_REVALIDATE_DASHBOARD! || 30
